63867d08dc5f91195646d48ae0e495a930e97744
danila032040 <7nexusdanila@gmail.com>
2022-11-01 12:54:11 +0100

    Added ModifiableEntities classes for testing

---
+++ ModifiableEntities/ModifiableEntities.EntityFrameworkCore/Extensions/ModifiableEntitiesDbContextExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace ModifiableEntities.EntityFrameworkCore.Extensions;
+
+/// <summary>
+///     Extension class needed for modifiable entity database contexts.
+/// </summary>
+public static class ModifiableEntitiesDbContextExtension
+{
+    /// <summary>
+    ///     Modifies entities last modification and creation dats according on there changes which were made previously.
+    /// </summary>
+    /// <param name="dbContext">DbContext on which will be applied this update method.</param>
+    public static void ModifyEntitiesOnSaveChanges(this DbContext dbContext)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ModifiableEntities/ModifiableEntities.EntityFrameworkCore/ModifiableEntitiesDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModifiableEntities.EntityFrameworkCore.Extensions;
+
+namespace ModifiableEntities.EntityFrameworkCore;
+
+/// <summary>
+///     <see cref="DbContext"/> for modifiable entities.
+/// </summary>
+public class ModifiableEntitiesDbContext : DbContext
+{
+    private readonly bool _useLazyLoading;
+
+    /// <summary>
+    ///     Initializes a new instance of the class.
+    /// </summary>
+    /// <param name="useLazyLoading">Set to true if you want to use lazy loading propagation.</param>
+    public ModifiableEntitiesDbContext(bool useLazyLoading = false)
+    {
+        _useLazyLoading = useLazyLoading;
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of the class.
+    /// </summary>
+    /// <param name="options">The options to be used by a <see cref="DbContext" />.</param>
+    /// <param name="useLazyLoading">Set to true if you want to use lazy loading propagation.</param>
+    public ModifiableEntitiesDbContext(DbContextOptions options, bool useLazyLoading = false) : base(options)
+    {
+        _useLazyLoading = useLazyLoading;
+    }
+
+    /// <inheritdoc />
+    public override int SaveChanges()
+    {
+        this.ModifyEntitiesOnSaveChanges();
+        return base.SaveChanges();
+    }
+
+    /// <inheritdoc />
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        this.ModifyEntitiesOnSaveChanges();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    /// <inheritdoc />
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        this.ModifyEntitiesOnSaveChanges();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        this.ModifyEntitiesOnSaveChanges();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (_useLazyLoading) optionsBuilder.UseLazyLoadingProxies();
+        base.OnConfiguring(optionsBuilder);
+    }
+    
+}
