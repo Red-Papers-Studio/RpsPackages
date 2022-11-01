27b0326fd1a5ff0f89008c5a216bceae0389301c
danila032040 <7nexusdanila@gmail.com>
2022-11-01 14:42:52 +0100

    Fixed test execution by using fixtures

---
+++ ModifiableEntities/ModifiableEntities.UnitTests/Fixtures/IdentityModifiableEntitiesDbContextFixture.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ModifiableEntities.Identity;
+using ModifiableEntities.UnitTests.Data;
+
+namespace ModifiableEntities.UnitTests.Fixtures;
+
+public class IdentityModifiableEntitiesDbContextFixture : IDisposable
+{
+    public DbContext DbContext { get; }
+    
+    public IdentityModifiableEntitiesDbContextFixture()
+    {
+        DbContextOptions<IdentityModifiableEntitiesDbContext<IdentityUser<int>, IdentityRole<int>, int>> dbOptions =
+            new DbContextOptionsBuilder<
+                    IdentityModifiableEntitiesDbContext<IdentityUser<int>, IdentityRole<int>, int>>()
+                .UseInMemoryDatabase("TestDB")
+                .Options;
+
+        DbContext = new TestingDbContext(dbOptions);
+
+        DbContext.Database.EnsureDeleted();
+        DbContext.Database.EnsureCreated();
+
+        DbContext.Set<TestEntity>().Add(new TestEntity());
+        DbContext.SaveChanges();
+    }
+    
+    ~IdentityModifiableEntitiesDbContextFixture()
+    {
+        Dispose(false);
+    }
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    
+    private void Dispose(bool disposing)
+    {
+        if (disposing) DbContext.Dispose();
+    }
+    
+    private class TestingDbContext : IdentityModifiableEntitiesDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        public TestingDbContext(bool useLazyLoading = false) : base(useLazyLoading)
+        {
+        }
+
+        public TestingDbContext(DbContextOptions options, bool useLazyLoading = false) : base(options, useLazyLoading)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Model.AddEntityType(typeof(TestEntity));
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ModifiableEntities/ModifiableEntities.UnitTests/Fixtures/ModifiableEntitiesDbContextFixture.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModifiableEntities.EntityFrameworkCore;
+using ModifiableEntities.UnitTests.Data;
+
+namespace ModifiableEntities.UnitTests.Fixtures;
+
+public class ModifiableEntitiesDbContextFixture : IDisposable
+{
+    public DbContext DbContext { get; }
+    
+    public ModifiableEntitiesDbContextFixture()
+    {
+        DbContextOptions<ModifiableEntitiesDbContext<int>> dbOptions =
+            new DbContextOptionsBuilder<ModifiableEntitiesDbContext<int>>()
+                .UseInMemoryDatabase("TestDB")
+                .Options;
+
+        DbContext = new TestingDbContext(dbOptions);
+
+        DbContext.Database.EnsureDeleted();
+        DbContext.Database.EnsureCreated();
+
+        DbContext.Set<TestEntity>().Add(new TestEntity());
+        DbContext.SaveChanges();
+    }
+    
+    ~ModifiableEntitiesDbContextFixture()
+    {
+        Dispose(false);
+    }
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    
+    private void Dispose(bool disposing)
+    {
+        if (disposing) DbContext.Dispose();
+    }
+    
+    private class TestingDbContext : ModifiableEntitiesDbContext<int>
+    {
+        public TestingDbContext(bool useLazyLoading = false) : base(useLazyLoading)
+        {
+        }
+
+        public TestingDbContext(DbContextOptions options, bool useLazyLoading = false) : base(options, useLazyLoading)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Model.AddEntityType(typeof(TestEntity));
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
