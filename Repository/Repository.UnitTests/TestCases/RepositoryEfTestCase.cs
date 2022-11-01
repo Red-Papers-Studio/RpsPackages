2871b636c8d13b6028f70fc4aacc30666dd8e506
danila032040 <7nexusdanila@gmail.com>
2022-11-01 16:58:03 +0100

    Code cleanup

--- Repository/Repository.UnitTests/TestCases/RepositoryEfTestCase.cs
+++ Repository/Repository.UnitTests/TestCases/RepositoryEfTestCase.cs
@@ -15,6 +15,147 @@ public class RepositoryEfTestCase : IClassFixture<RepositoryEfFixture>
         _repFixture = repFixture;
     }
 
+    [Fact]
+    public void CreateEntity_CorrectlyCreatesEntityInsideDbContext()
+    {
+        var exp = new TestEntity
+        {
+            Data = "Testing data creation"
+        };
+
+        _repFixture.Repository.Create(exp);
+        _repFixture.Repository.SaveChanges();
+
+        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
+        act.Should().Contain(exp);
+    }
+
+    [Fact]
+    public async void CreateAsyncEntity_CorrectlyCreatesEntityInsideDbContext()
+    {
+        var exp = new TestEntity
+        {
+            Data = "Testing data creation"
+        };
+
+        await _repFixture.Repository.CreateAsync(exp);
+        await _repFixture.Repository.SaveChangesAsync();
+
+        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
+        act.Should().Contain(exp);
+    }
+
+
+    [Fact]
+    public void ReadEntities_CorrectlyReadsEntitiesFromDbContext()
+    {
+        var exp = new List<TestEntity>
+        {
+            new()
+            {
+                Data = "Testing data reading 0"
+            },
+            new()
+            {
+                Data = "Testing data reading 1"
+            },
+            new()
+            {
+                Data = "Testing data reading 2"
+            },
+            new()
+            {
+                Data = "Testing data reading 3"
+            }
+        };
+
+        _repFixture.DbContext.Set<TestEntity>().AddRange(exp);
+        _repFixture.DbContext.SaveChanges();
+
+        List<TestEntity> act = _repFixture.Repository.Read().ToList();
+        act.Should().Contain(exp);
+    }
+
+    [Fact]
+    public void UpdateEntity_CorrectlyUpdatesEntityInsideDbContext()
+    {
+        var exp = new TestEntity
+        {
+            Data = "Testing data updating"
+        };
+
+
+        _repFixture.DbContext.Set<TestEntity>().Add(exp);
+        _repFixture.DbContext.SaveChanges();
+
+        exp.Data = "Testing data updating";
+
+        _repFixture.Repository.Update(exp);
+        _repFixture.Repository.SaveChanges();
+
+        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
+        act.Should().Contain(exp);
+    }
+
+    [Fact]
+    public async void UpdateAsyncEntity_CorrectlyUpdatesEntityInsideDbContext()
+    {
+        var exp = new TestEntity
+        {
+            Data = "Testing data updating"
+        };
+
+
+        await _repFixture.DbContext.Set<TestEntity>().AddAsync(exp);
+        await _repFixture.DbContext.SaveChangesAsync();
+
+        exp.Data = "Testing data updating";
+
+        await _repFixture.Repository.UpdateAsync(exp);
+        await _repFixture.Repository.SaveChangesAsync();
+
+        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
+        act.Should().Contain(exp);
+    }
+
+    [Fact]
+    public void DeleteEntity_CorrectlyDeleteEntityFromDbContext()
+    {
+        var deletingEntity = new TestEntity
+        {
+            Data = "Testing data deleting"
+        };
+
+
+        _repFixture.DbContext.Set<TestEntity>().Add(deletingEntity);
+        _repFixture.DbContext.SaveChanges();
+
+        _repFixture.Repository.Delete(deletingEntity);
+        _repFixture.Repository.SaveChanges();
+
+        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
+        act.Should().NotContain(deletingEntity);
+    }
+
+    [Fact]
+    public async void DeleteAsyncEntity_CorrectlyDeleteEntityFromDbContext()
+    {
+        var deletingEntity = new TestEntity
+        {
+            Data = "Testing data deleting"
+        };
+
+
+        _repFixture.DbContext.Set<TestEntity>().Add(deletingEntity);
+        await _repFixture.DbContext.SaveChangesAsync();
+
+        await _repFixture.Repository.DeleteAsync(deletingEntity);
+        await _repFixture.Repository.SaveChangesAsync();
+
+        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
+        act.Should().NotContain(deletingEntity);
+    }
+
     #region ArgumentNullException
 
     [Fact]
@@ -67,145 +208,4 @@ public class RepositoryEfTestCase : IClassFixture<RepositoryEfFixture>
     }
 
     #endregion
-
-    [Fact]
-    public void CreateEntity_CorrectlyCreatesEntityInsideDbContext()
-    {
-        var exp = new TestEntity
-        {
-            Data = "Testing data creation"
-        };
-
-        _repFixture.Repository.Create(exp);
-        _repFixture.Repository.SaveChanges();
-
-        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
-        act.Should().Contain(exp);
-    }
-    
-    [Fact]
-    public async void CreateAsyncEntity_CorrectlyCreatesEntityInsideDbContext()
-    {
-        var exp = new TestEntity
-        {
-            Data = "Testing data creation"
-        };
-
-        await _repFixture.Repository.CreateAsync(exp);
-        await _repFixture.Repository.SaveChangesAsync();
-
-        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
-        act.Should().Contain(exp);
-    }
-    
-
-    [Fact]
-    public void ReadEntities_CorrectlyReadsEntitiesFromDbContext()
-    {
-        var exp = new List<TestEntity>
-        {
-            new()
-            {
-                Data = "Testing data reading 0"
-            },
-            new()
-            {
-                Data = "Testing data reading 1"
-            },
-            new()
-            {
-                Data = "Testing data reading 2"
-            },
-            new()
-            {
-                Data = "Testing data reading 3"
-            }
-        };
-
-        _repFixture.DbContext.Set<TestEntity>().AddRange(exp);
-        _repFixture.DbContext.SaveChanges();
-        
-        List<TestEntity> act = _repFixture.Repository.Read().ToList();
-        act.Should().Contain(exp);
-    }
-
-    [Fact]
-    public void UpdateEntity_CorrectlyUpdatesEntityInsideDbContext()
-    {
-        var exp = new TestEntity
-        {
-            Data = "Testing data updating"
-        };
-
-
-        _repFixture.DbContext.Set<TestEntity>().Add(exp);
-        _repFixture.DbContext.SaveChanges();
-
-        exp.Data = "Testing data updating";
-
-        _repFixture.Repository.Update(exp);
-        _repFixture.Repository.SaveChanges();
-
-        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
-        act.Should().Contain(exp);
-    }
-    
-    [Fact]
-    public async void UpdateAsyncEntity_CorrectlyUpdatesEntityInsideDbContext()
-    {
-        var exp = new TestEntity
-        {
-            Data = "Testing data updating"
-        };
-
-
-        await _repFixture.DbContext.Set<TestEntity>().AddAsync(exp);
-        await _repFixture.DbContext.SaveChangesAsync();
-
-        exp.Data = "Testing data updating";
-
-        await _repFixture.Repository.UpdateAsync(exp);
-        await _repFixture.Repository.SaveChangesAsync();
-
-        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
-        act.Should().Contain(exp);
-    }
-
-    [Fact]
-    public void DeleteEntity_CorrectlyDeleteEntityFromDbContext()
-    {
-        var deletingEntity = new TestEntity
-        {
-            Data = "Testing data deleting"
-        };
-
-
-        _repFixture.DbContext.Set<TestEntity>().Add(deletingEntity);
-        _repFixture.DbContext.SaveChanges();
-
-        _repFixture.Repository.Delete(deletingEntity);
-        _repFixture.Repository.SaveChanges();
-
-        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
-        act.Should().NotContain(deletingEntity);
-    }
-    
-    [Fact]
-    public async void DeleteAsyncEntity_CorrectlyDeleteEntityFromDbContext()
-    {
-        var deletingEntity = new TestEntity
-        {
-            Data = "Testing data deleting"
-        };
-
-
-        _repFixture.DbContext.Set<TestEntity>().Add(deletingEntity);
-        await _repFixture.DbContext.SaveChangesAsync();
-
-        await _repFixture.Repository.DeleteAsync(deletingEntity);
-        await _repFixture.Repository.SaveChangesAsync();
-
-        DbSet<TestEntity> act = _repFixture.DbContext.Set<TestEntity>();
-        act.Should().NotContain(deletingEntity);
-    }
 }
